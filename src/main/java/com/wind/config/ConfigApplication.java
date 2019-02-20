bf38b69b4367631ef3fe54ea4732dc1af468dd18
qianchun <qianchun@huli.com>
2019-02-20 18:08:57 +0800

    test

--- src/main/java/com/wind/config/ConfigApplication.java
+++ src/main/java/com/wind/config/ConfigApplication.java
@@ -2,11 +2,12 @@ package com.wind.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class Application {
-
+public class ConfigApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ConfigApplication.class, args);
 	}
 }
